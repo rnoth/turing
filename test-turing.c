49b3f0020fcdb1244bb22f18583d23a8bd648b6b
moth <moth@airmail.cc>
2017-07-20 13:10:18 -0400

    test get_next_cell()

--- test-turing.c
+++ test-turing.c
@@ -3,6 +3,7 @@
 
 static void test_cell_alloc();
 static void test_cell_link();
+static void test_cell_traverse();
 static void test_free_tape();
 
 struct unit_test tests[] = {
@@ -10,6 +11,8 @@ struct unit_test tests[] = {
 	 .fun = unit_list(test_cell_alloc),},
 	{.msg = "should link cells",
 	 .fun = unit_list(test_cell_link),},
+	{.msg = "should traverse cells",
+	 .fun = unit_list(test_cell_traverse),},
 	{.msg = "should be able to free tapes",
 	 .fun = unit_list(test_free_tape),},
 };
@@ -52,6 +55,24 @@ test_cell_link()
 
 	ok(*b^(cell)a == (cell)c);
 	ok(*c == (cell)b);
+
+	try(free(a));
+	try(free(b));
+	try(free(c));
+}
+
+void
+test_cell_traverse()
+{
+	cell *a, *b, *c;
+
+	make_tape(&a, &b, &c);
+
+	ok(b == get_next_cell(a, 0));
+	ok(c == get_next_cell(b, a));
+	ok(0 == get_next_cell(c, b));
+	ok(0 == get_next_cell(a, b));
+	ok(a == get_next_cell(b, c));
 
 	try(free(a));
 	try(free(b));
