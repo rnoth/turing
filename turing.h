727459a50d40913c186e6b40f0834a6eba60eff9
moth <moth@airmail.cc>
2017-07-20 20:14:56 -0400

    document the cell type, draft the turing struct

--- turing.h
+++ turing.h
@@ -2,6 +2,20 @@
 #define _turing_
 #include <stdint.h>
 
+/*
+ * A Turing machine's tape is represented as a xor-linked list.
+ * There are two symbols, and these symbols are tagged into the
+ * link.
+ */
 typedef uintptr_t cell;
 
+typedef uint16_t state;
+typedef uint16_t action;
+
+struct turing {
+	cell  *current;
+	cell  *previous;
+	state  state;
+	action trans[];
+};
 #endif
